376f8352b163fcedf0d87f9c4a654de7f41c5d20
Igor Sansone <igor.sansone@crors.org.br>
2025-09-29 08:57:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -162,6 +162,7 @@ def generate_users():
     """Gera dicionário de usuários a partir da lista de colaboradores"""
     colaboradores = [
         ("André Nunes Flores", "13/05/1983"),
+        ("Alex Barcelos", "07/11/1975"),
         ("Ana Lopes", "01/02/1985"),
         ("Andréia Carla Viezzer", "08/06/1973"),
         ("Andressa Trápaga Paiz", "11/02/1990"),
